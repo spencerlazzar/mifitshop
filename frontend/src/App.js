c75b87ba1d1ac1c510e84feb0adaa3d306c16e10
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-04-29 10:04:58 -0700

    HomeScreen Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container} from 'react-bootstrap'
 import Header from './components/Header'
-
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-      <h1>Welcome to Pro Shop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
